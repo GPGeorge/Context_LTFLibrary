0f2466a88c1edceebe4f3b79b3b13781daa05fa4
George Hepworth <GPGeorge@users.noreply.github.com>
2025-09-10 05:44:37 -0700

    Public LTFCatalog files for Claude

---
+++ PublicationDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LTF_Library_V1.Pages
+{
+    public class PublicationDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
